b016a3776a8968bb6d25662ba629c7f9fa101683
MajikVroom <119984965+MajikVroom@users.noreply.github.com>
2024-07-18 22:14:16 -0700

    Create Location tab and put fake data in it. Purely in TextClient.

--- kvui.py
+++ kvui.py
@@ -374,6 +374,66 @@ class HintLabel(RecycleDataViewBehavior, BoxLayout):
         if self.index:
             self.selected = is_selected
 
+class LocationLabel(RecycleDataViewBehavior, BoxLayout):
+    selected = BooleanProperty(False)
+    striped = BooleanProperty(False)
+    index = None
+
+    def __init__(self):
+        super(LocationLabel, self).__init__()
+        self.region_text = ""
+        self.total_text = ""
+        self.found_text = ""
+        self.unfound_text = ""
+        for child in self.children:
+            child.bind(texture_size=self.set_height)
+
+    def set_height(self, instance, value):
+        self.height = max([child.texture_size[1] for child in self.children])
+
+    def refresh_view_attrs(self, rv, index, data):
+        self.index = index
+        self.striped = data.get("striped", False)
+        self.region_text = data["region"]["text"]
+        self.total_text = data["total"]["text"]
+        self.found_text = data["found"]["text"]
+        self.unfound_text = data["unfound"]["text"]
+        self.height = self.minimum_height
+        return super(LocationLabel, self).refresh_view_attrs(rv, index, data)
+
+    def on_touch_down(self, touch):
+        """ Add selection on touch down """
+        if super(LocationLabel, self).on_touch_down(touch):
+            return True
+        if self.index:  # skip header
+            # [Brian] In hints, this section lets you click to select a hint and put it in your copy/paste buffer.
+            pass
+        else:
+            parent = self.parent
+            parent.clear_selection()
+            parent: LocationLabel = parent.parent
+            # find correct column
+            for child in self.children:
+                if child.collide_point(*touch.pos):
+                    key = child.sort_key
+                    parent.hint_sorter = lambda element: remove_between_brackets.sub("", element[key]["text"]).lower()
+                    if key == parent.sort_key:
+                        # second click reverses order
+                        parent.reversed = not parent.reversed
+                    else:
+                        parent.sort_key = key
+                        parent.reversed = False
+                    break
+            else:
+                logging.warning("Did not find clicked header for sorting.")
+
+            App.get_running_app().update_hints()
+
+    def apply_selection(self, rv, index, is_selected):
+        """ Respond to the selection of items in the view. """
+        if self.index:
+            self.selected = is_selected
+
 
 class ConnectBarTextInput(TextInput):
     def insert_text(self, substring, from_undo=False):
@@ -534,6 +594,10 @@ class GameManager(App):
                 # show Archipelago tab if other logging is present
                 self.tabs.add_widget(panel)
 
+        location_panel = TabbedPanelItem(text="Locations")
+        self.log_panels["Locations"] = location_panel.content = LocationLog(self.json_to_kivy_parser)
+        self.tabs.add_widget(location_panel)
+
         hint_panel = TabbedPanelItem(text="Hints")
         self.log_panels["Hints"] = hint_panel.content = HintLog(self.json_to_kivy_parser)
         self.tabs.add_widget(hint_panel)
@@ -655,6 +719,12 @@ class GameManager(App):
     def update_hints(self):
         hints = self.ctx.stored_data[f"_read_hints_{self.ctx.team}_{self.ctx.slot}"]
         self.log_panels["Hints"].refresh_hints(hints)
+        fakeLocationData = [
+            {"region" : "Lake Hylia",    "total" : 4,  "found" : 3},
+            {"region" : "Lost Woods",    "total" : 3,  "found" : 3},
+            {"region" : "Forest Temple", "total" : 56, "found" : 0},
+        ]
+        self.log_panels["Locations"].refresh_locations(fakeLocationData)
 
     # default F1 keybind, opens a settings menu, that seems to break the layout engine once closed
     def open_settings(self, *largs):
@@ -707,7 +777,6 @@ class UILog(RecycleView):
         for element in self.children[0].children:
             if element.height != element.texture_size[1]:
                 element.height = element.texture_size[1]
-
 
 class HintLog(RecycleView):
     header = {
@@ -769,6 +838,47 @@ class HintLog(RecycleView):
             max_height = max(child.texture_size[1] for child in element.children)
             element.height = max_height
 
+
+class LocationLog(RecycleView):
+    header = {
+        "region": {"text": "[u]Region[/u]"},
+        "total": {"text": "[u]Total[/u]"},
+        "found": {"text": "[u]Found[/u]"},
+        "unfound": {"text": "[u]Unfound[/u]"},
+    }
+    
+    sort_key: str = ""
+    reversed: bool = False
+
+    def __init__(self, parser):
+        super(LocationLog, self).__init__()
+        self.data = [self.header]
+        self.parser = parser
+    
+    def refresh_locations(self, locations):
+        data = []
+        for location in locations:
+            data.append({
+                "region": {"text": self.parser.handle_node({"type": "text", "text": location["region"]})},
+                "total": {"text": self.parser.handle_node({"type": "text", "text": str(location["total"])})},
+                "found": {"text": self.parser.handle_node({"type": "text", "text": str(location["found"])})},
+                "unfound": {"text": self.parser.handle_node({"type": "color", "text": str(location["total"] - location["found"]),
+                                                             "color": "green" if location["total"] - location["found"] == 0 else "red" if location["found"] == 0 else "blue"})},
+            })
+
+        data.sort(key=self.hint_sorter, reverse=self.reversed)
+        data.insert(0, self.header)
+        self.data = data
+
+    @staticmethod
+    def hint_sorter(element: dict) -> str:
+        return ""
+
+    def fix_heights(self):
+        """Workaround fix for divergent texture and layout heights"""
+        for element in self.children[0].children:
+            max_height = max(child.texture_size[1] for child in element.children)
+            element.height = max_height
 
 class E(ExceptionHandler):
     logger = logging.getLogger("Client")
